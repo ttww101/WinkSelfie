658868f469b6d2551b88f10c8750a3faa8a849aa
amG <bomiishere@gmail.com>
2019-06-14 15:11:34 +0800

    Initial Commit

---
+++ WinkSelfie/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WinkSelfie
+//
+//  Created by Wu on 2019/6/14.
+//  Copyright © 2019 amG. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
